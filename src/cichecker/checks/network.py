4dcaf4b1f584ccb8634048820fe2d38b8bdd0490
richmr <richmr@users.noreply.github.com>
2024-06-03 20:10:29 -0700

    ping code

--- src/cichecker/checks/network.py
+++ src/cichecker/checks/network.py
@@ -108,4 +108,22 @@ def blockTest(
     CheckResponse
         A check response object
     """
-    return connectTest(dest_host, dest_port, protocol, timeout, check_block_instead=True)   
+    return connectTest(dest_host, dest_port, protocol, timeout, check_block_instead=True)
+
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+
+def ping(host):
+    """
+    NOT IMPLEMENTED
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0   
